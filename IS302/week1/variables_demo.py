aac792d1804400500bb0f43f9b91be2cd660e0a3
xvibeocampo <joeyaparejado68@gmail.com>
2026-04-10 12:41:22 +0800

    Add files via upload

---
+++ IS302/week1/variables_demo.py
@@ -0,0 +1,9 @@
+student_name = "Mj the Great"
+age = 19
+course = "BSIS"
+gpa = 1.75
+
+print("Student Name:", student_name)
+print("Age:", age_mja)
+print("Course:", course_mja)
+print("GPA:", gpa_mja)
